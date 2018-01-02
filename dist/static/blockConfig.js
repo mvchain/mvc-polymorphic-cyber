f0417c6b939e190491903b0f3ee0a079900cec1d
ningjianhao <375332835@qq.com>
2018-01-02 21:13:18 +0800

    build project

---
+++ dist/static/blockConfig.js
@@ -0,0 +1,8 @@
+import Web3 from 'web3';
+window.web3 = new Web3();
+let urlData = {
+  url: 'http://127.0.0.1',
+  port: '8082',
+  file: '/ethereum'
+};
+window.urlData = urlData;
---
+++ dist/static/js/babel-polyfill.ea0a20516e676bacc449.js
@@ -0,0 +1,2 @@
+webpackJsonp([5],[],["briU"]);
+//# sourceMappingURL=babel-polyfill.ea0a20516e676bacc449.js.map
